4967a0b0ec604bf178887a2975b6e2584e925718
dylen400mh <dylenbelanger2004@gmail.com>
2023-08-03 20:32:49 -0400

    Remove HTMLWebpackPlugin

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import App from "./app";
 
-App.getWeatherData("london")
+App.getWeatherData("london");
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,13 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
     index: "./src/index.js",
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Weather App",
-    }),
-  ],
   devtool: "inline-source-map",
   output: {
-    filename: "[name].bundle.js",
+    filename: "main.js",
     path: path.resolve(__dirname, "dist"),
-    clean: true,
   },
   module: {
     rules: [
