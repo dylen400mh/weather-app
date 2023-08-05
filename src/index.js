b5ddd59eb83af49fcae4c36485c60272c26cc5bd
dylen400mh <dylenbelanger2004@gmail.com>
2023-08-04 22:54:45 -0400

    Add check to handle scenario when coordinates are not found

--- src/index.js
+++ src/index.js
@@ -4,9 +4,16 @@ import Location from "./location";
 
 // get coordinates using GeoLocation API and use those coords to set default location on page load (units will be metric on default)
 Location.getCoordinates().then((coordinates) => {
-  const [latitude, longitude] = [coordinates[0], coordinates[1]];
+  // if coordinates were found, search with them, otherwise, search for a 'default' location
+  if (coordinates) {
+    const [latitude, longitude] = [coordinates[0], coordinates[1]];
 
-  App.getWeatherData(`${latitude},${longitude}`).then((data) => {
-    DOM.updateDisplay(data, "Metric");
-  });
+    App.getWeatherData(`${latitude},${longitude}`).then((data) => {
+      DOM.updateDisplay(data, "Metric");
+    });
+  } else {
+    App.getWeatherData("toronto").then((data) => {
+      DOM.updateDisplay(data, "Metric");
+    });
+  }
 });
